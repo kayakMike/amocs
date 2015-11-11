e6b5057131ba521841455df43dfed6c3276bfbcb
kayakMike <michael.lenczewski@gmail.com>
2015-11-12 09:21:45 +1030

    latest changes for neo pixel stuff

--- src/lpc1769/timer0.c
+++ src/lpc1769/timer0.c
@@ -11,33 +11,41 @@
 #define NRZ_ONE         200 
 #define NRZ_ZERO        100 
 #define NRZ_PERIOD      300 
-#define NRZ_PAUSE       120000
+#define NRZ_PAUSE       1200000
 
 //bytes to be transfered
 //index to the current bit
 volatile uint32_t nrz_index;
-volatile uint32_t nrz_values[NRZ_MAX+1];
-volatile uint8_t xmit_complete=true;
+volatile uint32_t nrz_values[NRZ_MAX];
+
+volatile uint8_t  nrz_xmit_complete=true;
 
 void Timer0_Handler(void){
-    TIMER0_INTERRUPT.match1=1; 
 
-    if(nrz_index<=NRZ_MAX){
+    if(TIMER0_MATCH1==NRZ_PAUSE){
+        nrz_xmit_complete=true;
+        TIMER0_CTL.enable=0;
+    }
+    else if(nrz_index<=NRZ_MAX){
         TIMER0_EXT_MATCH_CTL.em0=1;
         TIMER0_MATCH0=nrz_values[nrz_index];
         TIMER0_MATCH1=NRZ_PERIOD;
     }
     else{
+        TIMER0_EXT_MATCH_CTL.em0=1;
+        TIMER0_MATCH0=0;
         TIMER0_MATCH1=NRZ_PAUSE;
         nrz_index=0;
     }
+
     nrz_index++;
+    TIMER0_INTERRUPT.match1=1;
     
 }
 
 void nrz0_enable(void){
-    NVIC_SET_ENABLE.timer0=1;
-    NVIC_CLR_PENDING.timer0=1;
+//    NVIC_SET_ENABLE.timer0=1;
+//    NVIC_CLR_PENDING.timer0=1;
     TIMER0_MATCH1=NRZ_PERIOD;
     TIMER0_CTL.enable=1;
     TIMER1_CTL.reset=0;
@@ -45,9 +53,9 @@ void nrz0_enable(void){
 
 
 void nrz0_disable(void){
-//    while(xmit_complete==false){};
-    NVIC_SET_ENABLE.timer0=0;
-    NVIC_CLR_PENDING.timer0=0;
+    while(nrz_xmit_complete==false){};
+//    NVIC_SET_ENABLE.timer0=0;
+//    NVIC_CLR_PENDING.timer0=0;
     TIMER0_CTL.enable=0;
     TIMER0_CTL.reset=0;
 }
@@ -57,7 +65,7 @@ void nrz0_send_message(uint8_t *in, uint32_t len){
     uint8_t byte=0;
     uint8_t bit=0;
     uint8_t i=0;;
-   // nrz0_disable();
+    nrz0_disable();
     for(i=0;i<NRZ_MAX;i++){
         byte=i/8;
         bit=7-(i%8);
@@ -68,7 +76,8 @@ void nrz0_send_message(uint8_t *in, uint32_t len){
             nrz_values[i]=NRZ_ZERO;
         }
     }
-    //nrz0_enable();
+    nrz_xmit_complete=false;
+    nrz0_enable();
 }
 
 
@@ -81,12 +90,15 @@ void Timer0_Initialize(void){
     TIMER0_EXT_MATCH_CTL.em0=1;
     TIMER0_EXT_MATCH_CTL.mode0=1;
    
-    TIMER0_MATCH1=NRZ_PAUSE;
+    TIMER0_MATCH1=NRZ_PERIOD;
     TIMER0_INTERRUPT.match1=1; 
     TIMER0_MATCH_CTL.interrupt1=1;
     TIMER0_MATCH_CTL.reset1=1;
    
     PIN_FUNC.p3_25=2;
+    NVIC_SET_ENABLE.timer0=1;
+    NVIC_CLR_PENDING.timer0=1;
+    TIMER0_CTL.enable=0;
 
     //calculate_nrz();
 }
